59c62bdf90136b98ddfbcedce3b293143054ac28
yunaghgh <yunaghgh@naver.com>
2024-06-25 20:28:24 +0900

    [BE] create: 프로젝트 생성

---
+++ BE/src/main/java/com/twopilogue/intervyou/IntervYouApplication.java
@@ -0,0 +1,13 @@
+package com.twopilogue.intervyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntervYouApplication.class, args);
+    }
+
+}
---
+++ BE/src/test/java/com/twopilogue/intervyou/IntervYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twopilogue.intervyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntervYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
