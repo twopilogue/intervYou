072a591cd5eb533acc758af4448b658fd505ec6d
taboowiths <tabou9820@gmail.com>
2024-07-03 09:20:52 +0900

    [FE] feat: 네이버 소셜 로그인 연결

--- FE/pages/index.tsx
+++ FE/pages/index.tsx
@@ -1,18 +1,14 @@
-import axios from "axios";
+import { Button } from "./_components/button/Button";
+
+const REDIRECT_URL = process.env.NEXT_PUBLIC_REDIRECT_URL;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
 
 const HomePage = () => {
-  const test = () => {
-    axios
-      .get(process.env.NEXT_PUBLIC_API_BASE_URL + "/api/test")
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
-  };
+  const link = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&state=state`;
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div>
-        <button onClick={test}>버튼</button>
-      </div>
+      <Button as="a" href={link} label="로그인"></Button>
     </div>
   );
 };
