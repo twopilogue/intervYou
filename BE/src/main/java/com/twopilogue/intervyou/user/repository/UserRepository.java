d7d4ed5ad6dc684b826e67a6434f7a1bc0e5ee22
yunaghgh <yunaghgh@naver.com>
2024-07-03 14:17:06 +0900

    [BE] faet: 토큰으로 유저 조회 레포지토리

---
+++ BE/src/main/java/com/twopilogue/intervyou/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.twopilogue.intervyou.user.repository;
+
+import com.twopilogue.intervyou.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByNaverIdToken(final String naverIdToken);
+}
