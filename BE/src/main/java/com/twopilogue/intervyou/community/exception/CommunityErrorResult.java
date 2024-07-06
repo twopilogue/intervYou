2da08ac53a7402b312aa288b5b66e063ca0976a1
yunaghgh <yunaghgh@naver.com>
2024-07-06 17:16:08 +0900

    [BE] feat: 커뮤니티 예외처리 정의

---
+++ BE/src/main/java/com/twopilogue/intervyou/community/exception/CommunityErrorResult.java
@@ -0,0 +1,17 @@
+package com.twopilogue.intervyou.community.exception;
+
+import com.twopilogue.intervyou.common.BaseErrorResult;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommunityErrorResult implements BaseErrorResult {
+
+    INVALID_POST(HttpStatus.BAD_REQUEST, "유효하지 않은 게시글입니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
