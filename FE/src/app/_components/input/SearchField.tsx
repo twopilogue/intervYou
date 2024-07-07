6f576bcf894bbaccbae02e47858bbf4d57a5ec75
taboowiths <tabou9820@gmail.com>
2024-07-07 23:08:05 +0900

    [FE] design: SearchField CSS 수정

--- FE/src/app/_components/input/SearchField.tsx
+++ FE/src/app/_components/input/SearchField.tsx
@@ -1,23 +1,18 @@
 export const SearchField = ({ name, ...props }) => {
   return (
     <div className="flex items-center">
-      <i className="absolute p-2 box-border">
-        <svg
-          className="h-3 w-3 text-zinc-500 stroke-gray-80 "
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
+      <i className="absolute box-border p-4">
+        <svg className="text-zinc-500 stroke-gray-80 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2.5"
             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
           />
         </svg>
       </i>
       <input
-        className="w-full pl-6 pr-2.5 py-3 rounded-lg border border-gray-30 outline-none text-sm text-gray-90 focus:border-primary"
+        className="w-full rounded-lg border border-gray-30 py-3 pl-10 pr-2.5 text-sm text-gray-90 outline-none focus:border-primary"
         {...props}
       />
     </div>
