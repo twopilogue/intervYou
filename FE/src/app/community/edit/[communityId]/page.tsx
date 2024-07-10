a193b42eb0b767639f293bd67f7b5a782112ca20
taboowiths <tabou9820@gmail.com>
2024-07-10 09:23:27 +0900

    [FE] fix: 빌드 에러 수정

--- FE/src/app/community/edit/[communityId]/page.tsx
+++ FE/src/app/community/edit/[communityId]/page.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import CommunityCreate from "../../create/page";
+import CommunityCreate from "../../create/CommunityCreate";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
