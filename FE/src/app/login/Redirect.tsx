d3efbfbe2ad54dcb65b2e758908d4521ec234314
taboowiths <tabou9820@gmail.com>
2024-07-13 00:16:38 +0900

    [FE] fix: 웹소켓 에러 수정

--- FE/src/app/login/Redirect.tsx
+++ FE/src/app/login/Redirect.tsx
@@ -5,8 +5,9 @@ import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useAuthActions, useAuthStore } from "../../slices/auth.slice";
 import { io } from "socket.io-client";
+import { connectSocket } from "./Socket";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
 
 export default function Redirect({}) {
   const params = useSearchParams();
@@ -17,18 +18,21 @@ export default function Redirect({}) {
     const code = params.get("code");
     const res = await axios.get(`${BASE_URL}/api/users/login?code=${code}`);
     const { id: userId, nickname, token: accessToken } = res.data.data;
-    // login(userId, nickname, accessToken);
+    login(userId, nickname, accessToken);
     handleSocket(userId);
     router.push("/");
   };
 
   const handleSocket = (userId: number) => {
-    const socket = io(`${BASE_URL}/socket/notifications/${userId}`);
-    console.log(`${BASE_URL}/socket/notifications/${userId}`);
+    const socket = new WebSocket(
+      "ws://http://ec2-3-35-135-21.ap-northeast-2.compute.amazonaws.com:8080/socket/notifications/" + userId,
+    );
 
-    socket.on("connect", () => {
-      console.log(socket.id);
-    });
+    connectSocket(socket);
+
+    socket.onopen = () => {
+      console.log("connected");
+    };
   };
 
   useEffect(() => {
