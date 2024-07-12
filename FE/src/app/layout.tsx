899c18bc58de4907171411376359d6f066c64e06
taboowiths <tabou9820@gmail.com>
2024-07-12 22:58:07 +0900

    [FE] fix: Mixed Content 관련 에러 수정

--- FE/src/app/layout.tsx
+++ FE/src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   return (
     <html>
-      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <body className="h-svh w-svw">
         {pathname === "/chat" ? <ChatHeader /> : <Header />}
         <div className="h-[calc(100%-4rem)]">{children}</div>
