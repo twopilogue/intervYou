08b73ebc95dc0b1c1ba4903b6d144e1193428d23
taboowiths <tabou9820@gmail.com>
2024-07-18 09:29:49 +0900

    [FE] modify: 확인 모달 color type 재설정

--- FE/src/app/_components/modal/confirm/ModalConfirm.tsx
+++ FE/src/app/_components/modal/confirm/ModalConfirm.tsx
@@ -10,28 +10,36 @@ interface ModalConfirmProps {
 const info: {
   [key: string]: {
     message: string;
-    color: string;
+    color: "primary" | "secondary" | "gray" | "danger";
   };
 } = {
   save: {
     message: "등록하시겠습니까?",
-    color: "bg-primary",
+    color: "primary",
+  },
+  edit: {
+    message: "수정하시겠습니까?",
+    color: "primary",
+  },
+  delete: {
+    message: "삭제하시겠습니까?",
+    color: "danger",
   },
   withdraw: {
     message: "탈퇴하시겠습니까?",
-    color: "bg-danger-base",
+    color: "danger",
   },
 };
 
 const ModalConfirm = forwardRef(({ types, onClose, onConfirm }: ModalConfirmProps, ref) => {
   return (
-    <div className="shadow-3xl absolute left-2/4 top-2/4 flex h-48 w-64 -translate-x-2/4 -translate-y-2/4 flex-col items-center justify-center gap-8 rounded-2xl bg-white">
+    <div className="absolute left-2/4 top-2/4 flex h-48 w-64 -translate-x-2/4 -translate-y-2/4 flex-col items-center justify-center gap-8 rounded-2xl bg-white shadow-3xl">
       <span className="absolute top-16 text-sm">{info[types].message}</span>
-      <div className="absolute bottom-4 flex gap-4">
+      <div className="absolute bottom-4 flex gap-4 *:min-w-16">
         <Button types="gray" size="small" onClick={onClose}>
           취소
         </Button>
-        <Button size="small" onClick={onConfirm}>
+        <Button types={info[types].color} size="small" onClick={onConfirm}>
           확인
         </Button>
       </div>
